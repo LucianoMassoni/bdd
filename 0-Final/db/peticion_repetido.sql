6d9756f9b9387c151694a206b9397bba46a65b92
LucianoMassoni <massoni.luciano7@gmail.com>
2023-12-29 15:27:47 -0300

    correcciones

--- 0-Final/db/peticion_repetido.sql
+++ 0-Final/db/peticion_repetido.sql
@@ -15,3 +15,14 @@ join aeropuerto a_origen on v.origen = a_origen.codigo
 join aeropuerto a_destino on v.destino = a_destino.codigo
 left join vuelo_tripulacion vt on v.codigo = vt.codigo_vuelo
 left join tripulacion t on vt.codigo_tripulacion = t.codigo;
+
+-- uso del procedure y el trigger
+
+select t.nombre, t.apellido, t.rol, a.nombre, a.codigo
+from tripulacion as t, aeropuerto as a
+where t.codigo_aeropuerto = a.codigo;
+
+call crear_tripulante('juan', 'perez', 'copiloto', 4); -- el 4 es el codigo de ezeiza
+call crear_tripulante('pedro', 'gonzales', 'comisario de abordo', 1); -- el 1 es aeroparque
+call crear_tripulante('tomas', 'ramirez', 'tripulante de cabina', null); -- con el null entra en la funcion 
+call crear_tripulante('tomas', 'ramirez', 'tripulante de cabina', 2); -- si no es 1, 4 o null no se crea y tira un error
--- 0-Final/db/trigger.sql
+++ 0-Final/db/trigger.sql
@@ -18,3 +18,4 @@ end;
 //
 
 delimiter ;
+
